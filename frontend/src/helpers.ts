36330a42e0a304efaa4bb946d8fb104a5c71df35
ponkenman <gg7958209@gmail.com>
2025-04-07 23:57:18 +1000

    Add login screen functionality

---
+++ frontend/src/helpers.ts
@@ -0,0 +1,27 @@
+interface FetchOptions {
+  method: "GET"|"POST"|"PUT"|"DELETE",
+  headers: Record<string, string>,
+  body?: BodyInit
+}
+
+export async function fetchBackend(
+  httpMethod: "GET"|"POST"|"PUT"|"DELETE", 
+  urlFragment: string,
+  options?: Record<string, string>,
+  token?: string
+) {
+  const urlMain = "http://localhost:5005";
+  const fetchOptions: FetchOptions = {
+    method: httpMethod,
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: options ? JSON.stringify(options) : undefined
+  };
+  if (token) {
+    fetchOptions.headers["Authorization"] = token;
+  }
+
+  const response = await fetch(urlMain + urlFragment, fetchOptions);
+  return response.json();
+}
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
+import { fetchBackend } from "../helpers";
+
 export function HomePage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  async function login() {
+    const body = {
+      email: email,
+      password: password,
+      name: name,
+    }
+    const response = await fetchBackend("POST", "/admin/auth/login", body);
+    if (response.error) {
+      console.log(response);
+    } else {
+      console.log("Happy!");
+    }
+  }
+
+  function loginIfEnter(event: React.KeyboardEvent) {
+    if (event.key === "Enter") {
+      login();
+    }
+  }
+
   return (<>
     <h1> Login </h1>
     <form>
       <div>
         <label>Email</label>
-        <input></input>
+        <input onChange={e => setEmail(e.target.value)} onKeyDown={e => loginIfEnter(e)}></input>
       </div>
       <div>
         <label>Password</label>
-        <input></input>
+        <input onChange={e => setPassword(e.target.value)} onKeyDown={e => loginIfEnter(e)}></input>
       </div>
-      <button type="button"> Login</button>
+      <div>
+        <label>Name</label>
+        <input onChange={e => setName(e.target.value)} onKeyDown={e => loginIfEnter(e)}></input>
+      </div>
+      <button type="button" onClick={login}> Login</button>
     </form>
 
   </>);
