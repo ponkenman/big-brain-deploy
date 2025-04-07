785d30dbf15936aa07f20a613e1f0ee55f75f726
ponkenman <gg7958209@gmail.com>
2025-04-07 23:09:09 +1000

    Add typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from './pages/login';
+import { HomePage } from './pages/login.tsx';
 
 function App() {
   return (
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,17 @@
+export function HomePage() {
+  return (<>
+    <h1> Login </h1>
+    <form>
+      <div>
+        <label>Email</label>
+        <input></input>
+      </div>
+      <div>
+        <label>Password</label>
+        <input></input>
+      </div>
+      <button type="button"> Login</button>
+    </form>
+
+  </>);
+}
