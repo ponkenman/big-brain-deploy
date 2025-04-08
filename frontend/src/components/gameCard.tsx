aa466edde744f6d0389c7dcd1bd5400e8902dc4a
ponkenman <gg7958209@gmail.com>
2025-04-08 17:31:38 +1000

    Lint

--- frontend/src/components/gameCard.tsx
+++ frontend/src/components/gameCard.tsx
@@ -1,10 +1,10 @@
 export default function GameCard(props: {title: string, numQuestions: number, totalDuration: number}) {
-    return (<article>
+  return (<article>
     <p>{props.title}</p>
     <p>{props.numQuestions} Questions</p>
     <p>{props.totalDuration} seconds</p>
     <img src="" alt={`Thumbnail for ${props.title}`}></img>
     <button type="button">Edit game</button>
     <button type="button">Delete game</button>
-    </article>);
+  </article>);
 }
