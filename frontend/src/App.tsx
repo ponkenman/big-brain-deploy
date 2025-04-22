d2d8e303046e7d4d8b4b3aaa494156f9783fd949
ponkenman <gg7958209@gmail.com>
2025-04-22 20:45:49 +1000

    Protect routes if user does not have token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { LoginScreen } from './pages/login.tsx';
 import { DashboardScreen } from "./pages/dashboard.tsx";
 import { RegisterScreen } from "./pages/register.tsx";
@@ -11,10 +11,27 @@ import { ManageSessionScreen } from "./pages/manageSession.tsx";
 import { ResultsScreen } from "./pages/results.tsx";
 import { PlayerResultsScreen } from "./pages/playerResult.tsx";
 import { PastResultsScreen } from "./pages/pastResults.tsx";
-import { createContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import { AlertData, AlertMenu } from "./components/alert.tsx";
 
 export const AlertContext = createContext<(message: string, colour?: string) => void>(() => {});
+
+/** Wrapper which protects route if user does not have token */
+function PrivateRoute(props: { children: ReactNode }) {
+  const navigate = useNavigate();
+  const createAlert = useContext(AlertContext);
+
+  // Prevent from running twice in debug mode
+  let errorRendered = false;
+  useEffect(() => {
+    if (localStorage.getItem("token") === null && !errorRendered) {
+      errorRendered = true;
+      createAlert("You are not logged in!");
+      navigate(`/login`);
+    }
+  }, []);
+  return (props.children);
+}
 
 function App() {
   // Create global alerts system between pages on website
@@ -27,8 +44,8 @@ function App() {
       key: alertCount,
       colour: colour ?? undefined
     }
-    setAlerts(a => [...a, newAlert]);
     setAlertCount(c => c + 1);
+    setAlerts(a => [...a, newAlert]);
   }
 
   return (
@@ -39,13 +56,24 @@ function App() {
           <Route path="register" element={<RegisterScreen />}/>
           <Route path="join" element={<JoinGameScreen />}/>
           <Route path="play" element={<PlayGameScreen />}/>
-          <Route path="dashboard" element={<DashboardScreen />}/>
-          <Route path="game/:gameId/question/:questionId" element={<EditQuestionScreen />}/>
-          <Route path="game/:gameId" element={<EditGameScreen />}/>
-          <Route path="session/:sessionId" element={<ManageSessionScreen/>}/>
-          <Route path="session/:sessionId/results" element={<ResultsScreen/>}/>
           <Route path="play/:playerId/results" element={<PlayerResultsScreen/>}/>
           <Route path="/pastResults/:gameId" element={<PastResultsScreen/>}/>
+          {/** Private routes */}
+          <Route path="dashboard" element={<PrivateRoute>
+            <DashboardScreen />
+          </PrivateRoute>}/>
+          <Route path="game/:gameId/question/:questionId" element={<PrivateRoute>
+            <EditQuestionScreen />
+          </PrivateRoute>}/>
+          <Route path="game/:gameId" element={<PrivateRoute>
+            <EditGameScreen />
+          </PrivateRoute>}/>
+          <Route path="session/:sessionId" element={<PrivateRoute>
+            <ManageSessionScreen />
+          </PrivateRoute>}/>
+          <Route path="session/:sessionId/results" element={<PrivateRoute>
+            <ResultsScreen />
+          </PrivateRoute>}/>
           <Route path="/" element={<LoginRedirect />} />
           <Route path="" element={<LoginRedirect />} />
         </Routes>
